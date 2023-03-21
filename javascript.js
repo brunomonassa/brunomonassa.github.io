96824c6515430ddf2752ba92b3d9335adf18bed5
brunomonassa <106789942+brunomonassa@users.noreply.github.com>
2023-03-21 15:32:25 -0300

    inicial commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+document.getElementById('boton-de-color').addEventListener('click', function(){
+
+    document.body.style.backgroundColor = '#B7E8AD';
+
+});
+
+document.getElementById('fondo-original').addEventListener('click', function(){
+
+    document.body.style.backgroundColor = '#fff8dc';
+
+});
